a845c1cf414a23f981cd3e3174c57298f42c2c31
Sheraz2005 <sheraz2k5@gmail.com>
2024-10-27 16:20:13 +0530

    Update frontend.py
    start of frontend

--- frontend.py
+++ frontend.py
@@ -1,1 +1,10 @@
+from tkinter import *
+root=Tk()
+root.geometry("1650x700")
+root.title("Cafe Management System")
 
+e = Entry(root,width=30,borderwidth=5).grid(row=0, column=1, padx=10, pady=10)
+e.pack()
+
+myLabel=Label(root,text="enter your name").pack()
+root.mainloop()
